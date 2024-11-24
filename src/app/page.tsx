8276afd30656e415a7a1b7c99bda2605a174557e
SeungJooNoh <cyanosj128@gmail.com>
2024-11-24 09:28:02 +0900

    inspect round whenever round finishes drawing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,12 +12,19 @@ export default function Home() {
     }, 1000);
   }, []);
 
+  useEffect(() => {
+    if (round?.getHasDrawn()) {
+      round?.inspectRound();
+    }
+  }, [round?.getHasDrawn()]);
+
   if (!round) {
     return <div className="text-white">Please Wait</div>;
   }
 
   return (
     <div className="flex flex-col justify-center items-center gap-1">
+      <div className="text-white font-bold mb-[24px]">{round.getScore()}</div>
       {round.drawRound()}
     </div>
   );
