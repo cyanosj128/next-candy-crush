380dc2bdf31f25794b4d356abbd38b1f9fdb353b
SeungJooNoh <cyanosj128@gmail.com>
2024-11-24 09:28:29 +0900

    AbstractCandy returns color style

--- src/libs/candy/AbstractCandy.tsx
+++ src/libs/candy/AbstractCandy.tsx
@@ -4,6 +4,10 @@ import { Coordinate } from '../Coordinate';
 export abstract class AbstractCandy {
   constructor(private coord: Coordinate, private colorStyle: string) {}
 
+  getColorStyle() {
+    return this.colorStyle;
+  }
+
   draw() {
     return <BaseCandy key={this.coord.toKey()} bgStyle={this.colorStyle} />;
   }
